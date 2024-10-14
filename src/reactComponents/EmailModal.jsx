0d6729c406fa487f47689702e88edd453ae48d24
Nassim <jslegend@protonmail.com>
2024-10-14 17:34:30 -0400

    Update EmailModal.jsx

--- src/reactComponents/EmailModal.jsx
+++ src/reactComponents/EmailModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useAtom, useAtomValue } from "jotai";
 import {
   isEmailModalVisibleAtom,
