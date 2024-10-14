315ddf35156fca096178a24f70860f56c3eeeed2
Nassim <jslegend@protonmail.com>
2024-10-14 11:29:43 -0400

    implemented animations

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -8,7 +8,7 @@ import {
 
 export default function makePlayer(k, posVec2, speed) {
   const player = k.add([
-    k.sprite("player", { anim: "idle" }),
+    k.sprite("player", { anim: "walk-down" }),
     k.scale(8),
     k.anchor("center"),
     k.area({ shape: new k.Rect(k.vec2(0), 5, 5) }),
@@ -59,6 +59,62 @@ export default function makePlayer(k, posVec2, speed) {
       player.direction.y = -1;
     if (keyboardInput.isDownPressed || DPadInput.isDownPressed)
       player.direction.y = 1;
+
+    if (
+      player.direction.eq(k.vec2(1, 0)) &&
+      player.getCurAnim().name !== "walk-right"
+    ) {
+      player.play("walk-right");
+    }
+
+    if (
+      player.direction.eq(k.vec2(-1, 0)) &&
+      player.getCurAnim().name !== "walk-left"
+    )
+      player.play("walk-left");
+
+    if (
+      player.direction.eq(k.vec2(0, -1)) &&
+      player.getCurAnim().name !== "walk-up"
+    )
+      player.play("walk-up");
+
+    if (
+      player.direction.eq(k.vec2(0, 1)) &&
+      player.getCurAnim().name !== "walk-down"
+    )
+      player.play("walk-down");
+
+    if (
+      player.direction.eq(k.vec2(-1, -1)) &&
+      player.getCurAnim().name !== "walk-left-up"
+    )
+      player.play("walk-left-up");
+
+    if (
+      player.direction.eq(k.vec2(-1, 1)) &&
+      player.getCurAnim().name !== "walk-left-down"
+    )
+      player.play("walk-left-down");
+
+    if (
+      player.direction.eq(k.vec2(1, -1)) &&
+      player.getCurAnim().name !== "walk-right-up"
+    )
+      player.play("walk-right-up");
+
+    if (
+      player.direction.eq(k.vec2(1, 1)) &&
+      player.getCurAnim().name !== "walk-right-down"
+    )
+      player.play("walk-right-down");
+
+    if (
+      player.direction.eq(k.vec2(0, 0)) &&
+      !player.getCurAnim().name.includes("idle")
+    ) {
+      player.play(`${player.getCurAnim().name}-idle`);
+    }
 
     if (player.direction.x && player.direction.y) {
       player.move(player.direction.scale(DIAGONAL_FACTOR * speed));
--- src/initGame.js
+++ src/initGame.js
@@ -105,7 +105,22 @@ export default async function initGame() {
     sliceX: 4,
     sliceY: 8,
     anims: {
-      idle: 0,
+      "walk-down-idle": 0,
+      "walk-down": { from: 0, to: 3, loop: true },
+      "walk-left-down": { from: 4, to: 7, loop: true },
+      "walk-left-down-idle": 4,
+      "walk-left": { from: 8, to: 11, loop: true },
+      "walk-left-idle": 8,
+      "walk-left-up": { from: 12, to: 15, loop: true },
+      "walk-left-up-idle": 12,
+      "walk-up": { from: 16, to: 19, loop: true },
+      "walk-up-idle": 16,
+      "walk-right-up": { from: 20, to: 23, loop: true },
+      "walk-right-up-idle": 20,
+      "walk-right": { from: 24, to: 27, loop: true },
+      "walk-right-idle": 24,
+      "walk-right-down": { from: 28, to: 31, loop: true },
+      "walk-right-down-idle": 28,
     },
   });
   k.loadShaderURL("tiledPattern", null, "/shaders/tiledPattern.frag");
