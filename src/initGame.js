1db04c08dc91e78682e1d5067cff8f843ea3e327
Nassim <jslegend@protonmail.com>
2024-10-27 13:44:32 -0400

    added relative path to fix gh pages issue

--- src/initGame.js
+++ src/initGame.js
@@ -22,27 +22,27 @@ export default async function initGame() {
   ).json();
 
   const k = makeKaplayCtx();
-  k.loadFont("ibm-regular", "/fonts/IBMPlexSans-Regular.ttf");
-  k.loadFont("ibm-bold", "/fonts/IBMPlexSans-Bold.ttf");
-  k.loadSprite("github-logo", "/logos/github-logo.png");
-  k.loadSprite("linkedin-logo", "/logos/linkedin-logo.png");
-  k.loadSprite("youtube-logo", "/logos/youtube-logo.png");
-  k.loadSprite("x-logo", "/logos/x-logo.png");
-  k.loadSprite("substack-logo", "/logos/substack-logo.png");
-  k.loadSprite("javascript-logo", "/logos/javascript-logo.png");
-  k.loadSprite("typescript-logo", "/logos/typescript-logo.png");
-  k.loadSprite("react-logo", "/logos/react-logo.png");
-  k.loadSprite("nextjs-logo", "/logos/nextjs-logo.png");
-  k.loadSprite("postgres-logo", "/logos/postgres-logo.png");
-  k.loadSprite("html-logo", "/logos/html-logo.png");
-  k.loadSprite("css-logo", "/logos/css-logo.png");
-  k.loadSprite("tailwind-logo", "/logos/tailwind-logo.png");
-  k.loadSprite("python-logo", "/logos/python-logo.png");
-  k.loadSprite("email-logo", "/logos/email-logo.png");
-  k.loadSprite("sonic-js", "/projects/sonic-js.png");
-  k.loadSprite("kirby-ts", "/projects/kirby-ts.png");
-  k.loadSprite("platformer-js", "/projects/platformer-js.png");
-  k.loadSprite("player", "/sprites/player.png", {
+  k.loadFont("ibm-regular", "./fonts/IBMPlexSans-Regular.ttf");
+  k.loadFont("ibm-bold", "./fonts/IBMPlexSans-Bold.ttf");
+  k.loadSprite("github-logo", "./logos/github-logo.png");
+  k.loadSprite("linkedin-logo", "./logos/linkedin-logo.png");
+  k.loadSprite("youtube-logo", "./logos/youtube-logo.png");
+  k.loadSprite("x-logo", "./logos/x-logo.png");
+  k.loadSprite("substack-logo", "./logos/substack-logo.png");
+  k.loadSprite("javascript-logo", "./logos/javascript-logo.png");
+  k.loadSprite("typescript-logo", "./logos/typescript-logo.png");
+  k.loadSprite("react-logo", "./logos/react-logo.png");
+  k.loadSprite("nextjs-logo", "./logos/nextjs-logo.png");
+  k.loadSprite("postgres-logo", "./logos/postgres-logo.png");
+  k.loadSprite("html-logo", "./logos/html-logo.png");
+  k.loadSprite("css-logo", "./logos/css-logo.png");
+  k.loadSprite("tailwind-logo", "./logos/tailwind-logo.png");
+  k.loadSprite("python-logo", "./logos/python-logo.png");
+  k.loadSprite("email-logo", "./logos/email-logo.png");
+  k.loadSprite("sonic-js", "./projects/sonic-js.png");
+  k.loadSprite("kirby-ts", "./projects/kirby-ts.png");
+  k.loadSprite("platformer-js", "./projects/platformer-js.png");
+  k.loadSprite("player", "./sprites/player.png", {
     sliceX: 4,
     sliceY: 8,
     anims: {
@@ -64,7 +64,7 @@ export default async function initGame() {
       "walk-right-down-idle": 28,
     },
   });
-  k.loadShaderURL("tiledPattern", null, "/shaders/tiledPattern.frag");
+  k.loadShaderURL("tiledPattern", null, "./shaders/tiledPattern.frag");
 
   const setInitCamZoomValue = () => {
     if (k.width() < 1000) {
