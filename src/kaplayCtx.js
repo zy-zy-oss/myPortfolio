8b893da854380ad2abd825274838b06211c2dd8e
Nassim <jslegend@protonmail.com>
2024-10-27 15:54:05 -0400

    reverting due to pref improvement being negligible

--- src/kaplayCtx.js
+++ src/kaplayCtx.js
@@ -3,7 +3,7 @@ import kaplay from "kaplay";
 export default function makeKaplayCtx() {
   return kaplay({
     global: false,
-    pixelDensity: window.innerWidth < 1920 ? 1 : 2,
+    pixelDensity: 2,
     touchToMouse: true,
     debug: false,
     debugKey: "f1",
