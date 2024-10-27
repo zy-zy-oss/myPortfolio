522f787286532dcd7b7626e5e6a9becb21aff421
Nassim <jslegend@protonmail.com>
2024-10-27 12:27:50 -0400

    updated config for deployment on gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/new-2d-portfolio",
   plugins: [react()],
+  assetsInclude: ["**/*.ttf"],
 });
